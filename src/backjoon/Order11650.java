2a957ba76dfc1ba4bb0735e5c697b5f531f30b18
LeenaKim <tmxkvk94@naver.com>
2021-10-13 22:43:28 +0900

    Order

--- src/backjoon/Order11650.java
+++ src/backjoon/Order11650.java
@@ -2,6 +2,7 @@ package backjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 /*
  * 2차원 평면 위의 점 N개가 주어진다. 좌표를 x좌표가 증가하는 순으로, x좌표가 같으면 y좌표가 증가하는 순서로 정렬한 다음 출력하는 프로그램을 작성하시오.
  */
@@ -12,6 +13,7 @@ import java.io.InputStreamReader;
  * 첫째 줄부터 N개의 줄에 점을 정렬한 결과를 출력한다.
  */
 public class Order11650 {
+	/*
 	public static void heap(int[][] xy, int n) {
 		for(int i = 1; i < n; i++) {
 			int child = i;
@@ -68,4 +70,29 @@ public class Order11650 {
 			System.out.println();
 		}
 	}
+	*/
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[][] xy = new int[n][2];
+		
+		for(int i = 0; i < n; i++) {
+			String[] input = br.readLine().split(" ");
+			xy[i][0] = Integer.parseInt(input[0]);
+			xy[i][1] = Integer.parseInt(input[1]);
+		}
+		Arrays.sort(xy, (e1, e2) -> {
+			if(e1[0] == e2[0]) {
+				return e1[1] - e2[1];
+			} else {
+				return e1[0] - e2[0];
+			}
+		
+		});
+		
+		for(int i = 0; i < n; i++) {
+			System.out.println(xy[i][0] + " " + xy[i][1]);
+		}
+	}
+	
 }
