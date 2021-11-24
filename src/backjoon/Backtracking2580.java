056fd49fdea813182f8c8478e66f5c941b12d5f8
LeenaKim <tmxkvk94@naver.com>
2021-11-24 17:45:17 +0900

    Backtracking

--- src/backjoon/Backtracking2580.java
+++ src/backjoon/Backtracking2580.java
@@ -10,75 +10,67 @@ public class Backtracking2580 {
 
 	public static int[][] S = new int[9][9];
 	
-	public static boolean isLeftOne(int x, int y, int flag) {
-		boolean res = false;
-		boolean[] isNumUsed = new boolean[9];
+	public static boolean isLeftOne(int x, int y, int value) {
 		// 가로줄 
-		if(flag == 0) {
-			for(int i = 0; i < 9; i++) {
-				isNumUsed[S[x][i]] = true;
-				// 가로줄에 빈값이 본인 제외 또 있으면 
-				if(S[x][i] == 0 && i != y) {
-					break;
-				} else if(S[x][i] != 0 && i == 8) {
-					res = true;
-				}
+		for(int i = 0; i < 9; i++) {
+			// 가로줄에 곂치는 원소가 있는지 검사 
+			if(S[x][i] == value) {
+				return false;
 			}
+		}
 		// 세로줄 
-		} else if(flag == 1) {
-			for(int i = 0; i < 9; i++) {
-				isNumUsed[S[i][y]] = true;
-				// 세로줄에 빈값이 본인 제외 또 있으면 
-				if(S[i][y] == 0 && i != x) {
-					break;
-				} else if(S[i][y] != 0 && i == 8) {
-					res = true;
-				}
-			}
-		// 정사각형
-		} else {
-			for(int i = x / 3 * 3; i < x / 3 * 3 + 3; i++) {
-				for(int j = y / 3 * 3; j < y / 3 * 3 + 3; j++) {
-					isNumUsed[S[i][j]] = true;
-					if(S[i][j] == 0 && i != x && j != y) {
-						break;
-					} else if(S[i][j] != 0 && i == x / 3 * 3 + 2 && j == y / 3 * 3 + 2) {
-						res = true;
-					}
-				}
+		for(int i = 0; i < 9; i++) {
+			// 세로줄에 곂치는 원소가 있는지 검사 
+			if(S[i][y] == value) {
+				return false;
+			} 
+		}
+		// 정사각형 
+		for(int i = (x / 3) * 3; i < (x / 3) * 3 + 3; i++) {
+			for(int j = (y / 3) * 3; j < (y / 3) * 3 + 3; j++) {
+				if(S[i][j] == value) {
+					return false;
+				} 
 			}
 		}
 		
-		if(flag == 1 || flag == 0 && !res) {
-			isLeftOne(x, y, ++flag);
-		} else {
-			int leftNum = 0;
-			for(int i = 0; i < isNumUsed.length; i++) {
-				if(isNumUsed[i] == false) leftNum = i + 1;
-			}
-			S[x][y] = leftNum;
-			return res;
-		}
-		return res;
 		
+		return true; 
 	}
 	
-	public static void dfs(int fromX, int fromY, int depth) {
-		if(depth == 9) {
+	public static void dfs(int x, int y) {
+		// 열이 9로 모두 채워졌을 경우, 행을 바꿔 다시 호출 
+		if(y == 9) {
+			dfs(x + 1, 0);
 			return;
 		}
-		// 정사각형이나 가로줄, 세로줄 중 빈 숫자가 하나인것 먼저 채움 
-		// 하나이지 않으면 다음 빈칸으로 넘어가고 다른것부터 
-		// 다시 돌아와서 못채운것 채움 
-		for(int i = fromX; i < 9; i++) {
-			for(int j = fromY; j < 9; j++) {
-				if(S[i][j] == 0) {
-					if(!isLeftOne(i, j, 0)) {
-						dfs(i+1, j+1, depth + 1);						
-					}
+		
+		// 행이 9로 모두 채워졌을 경우, 배열을 출력 
+		if(x == 9) {
+			StringBuilder sb = new StringBuilder();
+			for(int i = 0; i < 9; i++) {
+				for(int j = 0; j < 9; j++) {
+					sb.append(S[i][j]).append(' ');
+				}
+				sb.append('\n');
+			}
+			System.out.print(sb);
+			System.exit(0); // 프로그램 종료 
+		}
+		
+		// 해당 위치가 0이라면 1부터 9까지 중 가능한 수 탐색 
+		if(S[x][y] == 0) {
+			for(int i = 1; i <= 9; i++) {
+				if(isLeftOne(x, y, i)) {
+					S[x][y] = i; // 해당 수가 가능하다면 배열에 할당하고 열을 늘려 재귀호출 
+					dfs(x, y+1);
 				}
 			}
+			S[x][y] = 0;
+			return;
 		}
+		// 0이 아니면 열을 하나씩 늘려 다시 탐색 
+		dfs(x, y + 1);
 	}
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -88,12 +80,6 @@ public class Backtracking2580 {
 				S[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		dfs(0, 0, 0);
-		for(int i = 0; i < S.length; i++) {
-			for(int j = 0; j < S[i].length; j++) {
-				System.out.print(S[i][j] + " ");
-			}
-			System.out.println();
-		}
+		dfs(0, 0);
 	}
 }
