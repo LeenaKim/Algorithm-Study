b312b6f1da54f832c125265a9d21079ff16440f1
LeenaKim <tmxkvk94@naver.com>
2021-12-23 22:57:40 +0900

    DP

--- src/backjoon/dp/DP2579.java
+++ src/backjoon/dp/DP2579.java
@@ -10,44 +10,35 @@ import java.util.StringTokenizer;
 public class DP2579 {
 
 	public static int[] scores;
-	public static int[] stepsMemo;		// 누적합을 저장할 배열 
-	public static boolean[] visited;	// 연속 계단을 올랐는지 판단을 위한 배열 
-	
-	public static boolean isContinued(int n) {
-		if(visited[n - 1] && visited[n + 1]) {
-			return true;
-		} else {
-			return false;
-		}
-	}
+	public static Integer[] stepsMemo;		// 누적합을 저장할 배열 
 	
 	public static int getMaxScores(int n) {
-		if(stepsMemo[n] == 0) {
-			if(!visited[n]) {
-				visited[n] = true;
-				if(!isContinued(n)) {
-					
-				} else {
-					getMaxScores(n - 1);
-				}
-			}
-		}
-		return stepsMemo[n];
+			
+		if(stepsMemo[n] == null) {
+			// 2계단 아래의 점수와, 3계단 아래의 점수 + 1계단 아래의 점수 중 큰 값에, 현재 계단의 점수를 합한 것 
+			stepsMemo[n] = Math.max(getMaxScores(n - 2), getMaxScores(n - 3) + scores[n - 1]) + scores[n];
+		} 
+		return stepsMemo[n];				
+		
 	}
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		scores = new int[n];
-		stepsMemo = new int[n];
-		visited = new boolean[n];
+		scores = new int[n + 1];
+		stepsMemo = new Integer[n + 1];
 		
-		for(int i = 0; i < n; i++) {
+		for(int i = 1; i <= n; i++) {
 			scores[i] = Integer.parseInt(br.readLine());
 		}
-		// 초깃값 세팅 = 1번째 계단 
-		stepsMemo[0] = scores[0];
-		
-		getMaxScores(n-1);
+		// 초깃값 세팅 
+		stepsMemo[0] = scores[0];	// 시작점 
+		stepsMemo[1] = scores[1];	// 1번째 계단 
+
+		// n = 1 인 경우를 대비해 예외처리 -> n 이 2 이상일 경우 
+		if(n >= 2) {
+			stepsMemo[2] = scores[1] + scores[2];
+		}
+		System.out.println(getMaxScores(n));
 	}
 }
